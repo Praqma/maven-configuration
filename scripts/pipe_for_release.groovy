be1b6df11d3417096c40f5c688c3ac0b34624662
Praqma Release User <man@praqma.net>
2018-06-21 16:22:51 +0200

    Move to functions

--- scripts/pipe_for_release.groovy
+++ scripts/pipe_for_release.groovy
@@ -1,40 +1,37 @@
 properties([parameters([booleanParam(defaultValue: false, description: '', name: 'isRelease')])])
+
+//TODO: We could put this in a shared library
+def buildMaven(args = [:]) {
+    git credentialsId: args.credentialsId ?: 'github', url: args.repo
+    docker.image(args.buildImage ?: "maven:3.5.3-jdk-8").inside("-v maven-repo:/root/.m2") {
+        sh args.buildCommand ?: "mvn clean package"
+    }    
+}
+
+//TODO: We could put this in a shared library
+def releaseMaven(args = [:]) {
+    sh 'curl https://raw.githubusercontent.com/Praqma/maven-info/master/settings.xml -O'
+    withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'passRelease', usernameVariable: 'userRelease'), string(credentialsId: 'jenkins-artifactory', variable: 'RELEASE_PW')]) {
+        docker.image(args.buildImage ?: "maven:3.5.3-jdk-8").inside("-v maven-repo:/root/.m2") {
+            sh 'git config user.email "release@praqma.net" && git config user.name "Praqma Release User"'
+            sh 'mvn clean release:prepare release:perform -B -s settings.xml -Dusername=$userRelease -Dpassword=$passRelease'
+        }            
+    }    
+}
+
 stage("build") {
-    if(!params?.isRelease) {
-        node("dockerhost1") {
-            git credentialsId: 'github', url: 'https://github.com/Praqma/drmemory-plugin.git'
-            docker.image("maven:3.5.3-jdk-8").inside("-v maven-repo:/root/.m2") {
-                sh 'mvn clean package'
-            }
-        }
-    } else {
-        echo "Release build is enabled. Skipping regular build."
+    node("dockerhost1") {
+        buildMaven(repo: 'https://github.com/Praqma/drmemory-plugin.git', buildCommand: 'mvn clean package')
     }
 }
 
-
-
-/**
-    Requirements
-        1. HTTPS developer and SCM connections in .pom
-
-    TODOs:
-        1. We need to not delete the /root folder every time (deletes the volume we just mounted)
-        2. We need to use a specific version of the maven docker image
-**/
 stage("release") {
     if(!params?.isRelease) {
         echo "Release build is not enabled"
     } else {
         echo "Release build enabled. Running release."
         node("dockerhost1") {
-            sh 'curl https://raw.githubusercontent.com/Praqma/maven-info/master/settings.xml -O'
-            withCredentials([usernamePassword(credentialsId: 'github', passwordVariable: 'passRelease', usernameVariable: 'userRelease'), string(credentialsId: 'jenkins-artifactory', variable: 'RELEASE_PW')]) {
-                docker.image("maven:3.5.3-jdk-8").inside("-v maven-repo:/root/.m2") {
-                    sh 'git config user.email "release@praqma.net" && git config user.name "Praqma Release User"'
-                    sh 'mvn clean release:prepare release:perform -B -s settings.xml -Dusername=$userRelease -Dpassword=$passRelease'
-                }            
-            }
+            releaseMaven()
         }
     }
 }
